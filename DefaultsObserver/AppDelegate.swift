eea3374d3122fd2e4a6a9d126f7df573c1b96ce1
Kaunteya <k.suryawanshi@gmail.com>
2020-05-15 17:11:14 +0530

    Initial Commit

---
+++ DefaultsObserver/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DefaultsObserver
+//
+//  Created by Kaunteya Suryawanshi on 15/05/20.
+//  Copyright © 2020 Kaunteya Suryawanshi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DefaultsObserver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DefaultsObserver
+//
+//  Created by Kaunteya Suryawanshi on 15/05/20.
+//  Copyright © 2020 Kaunteya Suryawanshi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
