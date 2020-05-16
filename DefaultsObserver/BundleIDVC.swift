8e3800b94d385b4b1fcf49ab369e7e205f8524c5
Kaunteya <k.suryawanshi@gmail.com>
2020-05-16 13:33:42 +0530

    Bundles are stored

--- DefaultsObserver/BundleIDVC.swift
+++ DefaultsObserver/BundleIDVC.swift
@@ -1,16 +1,41 @@
 import Cocoa
 
+let defaults = UserDefaults.standard
+
 class BundleIDVC: NSViewController {
-    @IBOutlet var bundleTextField: NSTextField!
+    @IBOutlet var comboBox: NSComboBox!
     override func prepare(for segue: NSStoryboardSegue, sender: Any?) {
         let wc = segue.destinationController as! NSWindowController
         let vc = wc.contentViewController as! PinnedVC
-        vc.bundle = BundleID(bundleTextField.stringValue)
+        let id = comboBox.stringValue
+        vc.bundle = BundleID(id)
         self.view.window?.close()
+        storeBundleIdInUserDefaults(id)
     }
-    
+
+    let userDefaultsKey = "storedBundleIds"
+
+    func storeBundleIdInUserDefaults(_ id: String) {
+        defaults.removeObject(forKey: userDefaultsKey)
+        var old = defaults.stringArray(forKey: userDefaultsKey) ?? []
+        old.append(id)
+        old = Array(Set(old)).sorted()
+        defaults.set(old, forKey: userDefaultsKey)
+    }
+
     override func viewDidAppear() {
         super.viewDidAppear()
         self.view.window?.center()
+    }
+}
+
+extension BundleIDVC: NSComboBoxDataSource, NSComboBoxDelegate {
+    func numberOfItems(in comboBox: NSComboBox) -> Int {
+        defaults.stringArray(forKey: userDefaultsKey)?.count ?? 0
+    }
+
+    func comboBox(_ comboBox: NSComboBox, objectValueForItemAt index: Int) -> Any? {
+        let all = defaults.stringArray(forKey: userDefaultsKey) ?? []
+        return all[index]
     }
 }
